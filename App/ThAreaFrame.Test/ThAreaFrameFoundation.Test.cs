63aeaffef382f4a1baced67ef8d687a38f6d3299
Jerry Dong <shichongdong80@163.com>
2019-06-24 12:06:00 +0800

    Fix ThAreaFrame NUnit test cases

--- App/ThAreaFrame.Test/ThAreaFrameFoundation.Test.cs
+++ App/ThAreaFrame.Test/ThAreaFrameFoundation.Test.cs
@@ -12,7 +12,7 @@ namespace ThAreaFrame.Test
         [Test]
         public void CreateObject()
         {
-            ThAreaFrameFoundation foundation = ThAreaFrameFoundation.Foundation(@"单体基底_1_1号单体_混合_4_2_2.8%-2^-1'2!4%1!4.5%3'4!3.6_0_0.45_1.2_无_V2.2");
+            ThAreaFrameFoundation foundation = ThAreaFrameFoundation.Foundation(@"单体基底_1_1号单体_混合_4_2_2.8%-2^-1'2!4%1!4.5%3'4!3.6_0_0.45_1.2_是__V2.2");
             Assert.AreEqual(foundation.numberID, @"1");
             Assert.AreEqual(foundation.name, @"1号单体");
             Assert.AreEqual(foundation.category, @"混合");
@@ -21,7 +21,7 @@ namespace ThAreaFrame.Test
         [Test]
         public void StoreyHeight()
         {
-            ThAreaFrameFoundation foundation = ThAreaFrameFoundation.Foundation(@"单体基底_1_1号单体_混合_4_2_2.8%-2^-1'2!4%1!4.5%3'4!3.6_0_0.45_1.2_无_V2.2");
+            ThAreaFrameFoundation foundation = ThAreaFrameFoundation.Foundation(@"单体基底_1_1号单体_混合_4_2_2.8%-2^-1'2!4%1!4.5%3'4!3.6_0_0.45_1.2_是__V2.2");
             Assert.AreEqual(foundation.StoreyHeight(-2), 4.0);
             Assert.AreEqual(foundation.StoreyHeight(-1), 4.0);
             Assert.AreEqual(foundation.StoreyHeight(1), 4.5);
