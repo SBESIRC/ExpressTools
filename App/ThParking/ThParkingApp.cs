cd555f2854b07767e5a5b9372b6f7a4ec7bc02e8
Jerry Dong <shichongdong80@163.com>
2019-06-24 16:11:08 +0800

    Application Initialization and Load-Time Optimization for ThParking module https://knowledge.autodesk.com/search-result/caas/CloudHelp/cloudhelp/2017/ENU/AutoCAD-NET/files/GUID-FA3B4125-F7BD-4E89-969F-9DCC90AC6977-htm.html

--- App/ThParking/ThParkingApp.cs
+++ App/ThParking/ThParkingApp.cs
@@ -11,56 +11,37 @@ using System.Linq;
 using System.Text;
 using AcadApp = Autodesk.AutoCAD.ApplicationServices.Application;
 
+[assembly: CommandClass(typeof(TianHua.AutoCAD.Parking.ThParkingCommands))]
+[assembly: ExtensionApplication(typeof(TianHua.AutoCAD.Parking.ThParkingApp))]
 
 namespace TianHua.AutoCAD.Parking
 {
     public class ThParkingApp : IExtensionApplication
     {
-        const string CMD_GROUPNAME = "TIANHUACAD";
-        const string CMD_THCNU_GLOBAL_NAME = "THCNU";
-
         public void Initialize()
         {
-            //将程序有效期验证为3个月，一旦超过时限，要求用户更新，不进行命令注册
-            var usualDate = new DateTime(2019, 6, 1);
-            var dateTime = DateTime.Today;
-            if ((dateTime - usualDate).Days <= 62)
-            {
-                //注册命令
-                RegisterCommands();
-            }
         }
 
         public void Terminate()
         {
-            UnregisterCommands();
-
         }
+    }
 
-        public void RegisterCommands()
-        {
-            //注册车位编号命令
-            Utils.AddCommand(CMD_GROUPNAME, CMD_THCNU_GLOBAL_NAME, CMD_THCNU_GLOBAL_NAME, CommandFlags.Modal, new CommandCallback(ShowNumberDialog));
-        }
-
-        public void UnregisterCommands()
-        {
-            //反注册车位编号命令
-            Utils.RemoveCommand(CMD_GROUPNAME, CMD_THCNU_GLOBAL_NAME);
-        }
-
+    public class ThParkingCommands
+    {
+        [CommandMethod("TIANHUACAD", "THCNU", CommandFlags.Modal)]
         public void ShowNumberDialog()
         {
+            //将程序有效期验证为3个月，一旦超过时限，要求用户更新，不进行命令注册
+            var usualDate = new DateTime(2019, 6, 1);
+            var dateTime = DateTime.Today;
+            if ((dateTime - usualDate).Days > 62)
+            {
+                return;
+            }
+
             ThParkingDialog parkingDialog = ThParkingDialog.GetInstance();
             AcadApp.ShowModalWindow(parkingDialog);
-            //if (parkingDialog.ResultState == true)
-            //{
-            //    var numberManager = new ThNumberingManager();
-            //    numberManager.Numbering(parkingDialog);
-            //}
         }
-
-
-
     }
 }
