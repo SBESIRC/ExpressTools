21afe8ee81e94a0adcaec351227a4561875ecfa8
Jerry Dong <shichongdong80@163.com>
2019-06-26 15:33:24 +0800

    Implement NUnit test cases for new model classes

---
+++ App/ThAreaFrame.Test/ThAreaFrameCommon.Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ThAreaFrame.Test
+{
+    [TestFixture]
+    class ThAreaFrameCommonTest
+    {
+        [Test]
+        public void CreateObject()
+        {
+            ThAreaFrameCommon common = ThAreaFrameCommon.Common(@"公摊_1_走廊_c1_A_HT1_v2.1");
+            Assert.AreEqual(common.type, @"公摊");
+            Assert.AreEqual(common.areaFactor, @"1");
+            Assert.AreEqual(common.name, @"走廊");
+        }
+    }
+}
---
+++ App/ThAreaFrame.Test/ThAreaFrameRoofGreenSpace.Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ThAreaFrame.Test
+{
+    [TestFixture]
+    class ThAreaFrameRoofGreenSpaceTest
+    {
+        [Test]
+        public void CreateObject()
+        {
+            ThAreaFrameRoofGreenSpace roofGreenSpace = ThAreaFrameRoofGreenSpace.RoofOfGreenSpace(@"屋顶构件_屋顶绿地_0.5_v2.1");
+            Assert.AreEqual(roofGreenSpace.type, @"屋顶构件");
+            Assert.AreEqual(roofGreenSpace.areaType, @"屋顶绿地");
+            Assert.AreEqual(roofGreenSpace.coefficient, @"0.5");
+        }
+    }
+}
