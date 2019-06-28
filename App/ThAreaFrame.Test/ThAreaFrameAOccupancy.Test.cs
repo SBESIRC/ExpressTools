b2ad82c67b0cf44219d1366a24fddfe543f66e19
Jerry Dong <shichongdong80@163.com>
2019-06-28 10:45:08 +0800

    Implement the model class, ThAreaFrameAOccupancy.

---
+++ App/ThAreaFrame.Test/ThAreaFrameAOccupancy.Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ThAreaFrame.Test
+{
+    [TestFixture]
+    class ThAreaFrameAOccupancyTest
+    {
+        [Test]
+        public void CreateObjects()
+        {
+            ThAreaFrameAOccupancy main = ThAreaFrameAOccupancy.Main("附属公建_主体_教育_幼儿园_1_1_0_c1^3_PUB1_v2.1");
+            Assert.AreEqual(main.areaType, "主体");
+            Assert.AreEqual(main.publicAreaID, "PUB1");
+
+            ThAreaFrameAOccupancy balcony = ThAreaFrameAOccupancy.Balcony("附属公建_阳台_教育_幼儿园_1_1_c1^3_PUB1_v2.1");
+            Assert.AreEqual(balcony.areaType, "阳台");
+            Assert.AreEqual(balcony.publicAreaID, "PUB1");
+
+            ThAreaFrameAOccupancy stilt = ThAreaFrameAOccupancy.Stilt("附属公建_架空_教育_幼儿园_1_1_1_c1_v2.1");
+            Assert.AreEqual(stilt.areaType, "架空");
+            Assert.AreEqual(stilt.storeys, "c1");
+
+            ThAreaFrameAOccupancy barWindow = ThAreaFrameAOccupancy.Baywindow("附属公建_飘窗_教育_小学_0.5_0.5_c1^3_v2.1");
+            Assert.AreEqual(barWindow.areaType, "飘窗");
+            Assert.AreEqual(barWindow.storeys, "c1^3");
+
+            ThAreaFrameAOccupancy rainshed = ThAreaFrameAOccupancy.Rainshed("附属公建_雨棚_教育_小学_0.5_0.5_c1^3_v2.1");
+            Assert.AreEqual(rainshed.areaType, "雨棚");
+            Assert.AreEqual(rainshed.storeys, "c1^3");
+
+            ThAreaFrameAOccupancy miscellaneous = ThAreaFrameAOccupancy.Miscellaneous("附属公建_附属其他构件_通风井_室内停车库_小型汽车_1_0_c1_v2.1");
+            Assert.AreEqual(miscellaneous.areaType, "附属其他构件");
+            Assert.AreEqual(miscellaneous.storeys, "c1");
+        }
+    }
+}
---
+++ App/ThAreaFrame/ThAreaFrameAOccupancy.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ThAreaFrame
+{
+    public class ThAreaFrameAOccupancy
+    {
+        // 附属公建
+        public string type;
+
+        // 
+        public string areaType;
+
+        // 类型
+        public string occupancyType;
+
+        // 构件名称
+        public string component;
+
+        // 性质
+        public string category;
+
+        // 计算系数
+        public string areaRatio;
+
+        // 计容系数
+        public string floorAreaRatio;
+
+        // 车位层数
+        public string parkingStoreys;
+
+        // 所属层
+        public string storeys;
+
+        // 公摊标识
+        public string publicAreaID;
+
+        // 版本号
+        public string version;
+
+        // 图层名
+        public string layer;
+
+        public ThAreaFrameAOccupancy(string name)
+        {
+            layer = name;
+        }
+
+        // 主体
+        public static ThAreaFrameAOccupancy Main(string name)
+        {
+            string[] tokens = name.Split('_');
+            ThAreaFrameAOccupancy main = new ThAreaFrameAOccupancy(name)
+            {
+                type                = tokens[0],
+                areaType            = tokens[1],
+                occupancyType       = tokens[2],
+                category            = tokens[3],
+                areaRatio           = tokens[4],
+                floorAreaRatio      = tokens[5],
+                parkingStoreys      = tokens[6],
+                storeys             = tokens[7],
+                publicAreaID        = tokens[8],
+                version             = tokens[9]
+            };
+            return main;
+        }
+
+        // 阳台
+        public static ThAreaFrameAOccupancy Balcony(string name)
+        {
+            string[] tokens = name.Split('_');
+            ThAreaFrameAOccupancy balcony = new ThAreaFrameAOccupancy(name)
+            {
+                type                = tokens[0],
+                areaType            = tokens[1],
+                occupancyType       = tokens[2],
+                category            = tokens[3],
+                areaRatio           = tokens[4],
+                floorAreaRatio      = tokens[5],
+                storeys             = tokens[6],
+                publicAreaID        = tokens[7],
+                version             = tokens[8]
+
+            };
+            return balcony;
+        }
+
+        // 架空
+        public static ThAreaFrameAOccupancy Stilt(string name)
+        {
+            string[] tokens = name.Split('_');
+            ThAreaFrameAOccupancy open = new ThAreaFrameAOccupancy(name)
+            {
+                type                = tokens[0],
+                areaType            = tokens[1],
+                occupancyType       = tokens[2],
+                category            = tokens[3],
+                areaRatio           = tokens[4],
+                floorAreaRatio      = tokens[5],
+                parkingStoreys      = tokens[6],
+                storeys             = tokens[7],
+                version             = tokens[8]
+
+            };
+            return open;
+        }
+
+        // 飘窗
+        public static ThAreaFrameAOccupancy Baywindow(string name)
+        {
+            string[] tokens = name.Split('_');
+            ThAreaFrameAOccupancy baywindow = new ThAreaFrameAOccupancy(name)
+            {
+                type                = tokens[0],
+                areaType            = tokens[1],
+                occupancyType       = tokens[2],
+                category            = tokens[3],
+                areaRatio           = tokens[4],
+                floorAreaRatio      = tokens[5],
+                storeys             = tokens[6],
+                version             = tokens[7]
+
+            };
+            return baywindow;
+        }
+
+        // 雨棚
+        public static ThAreaFrameAOccupancy Rainshed(string name)
+        {
+            string[] tokens = name.Split('_');
+            ThAreaFrameAOccupancy rainshed = new ThAreaFrameAOccupancy(name)
+            {
+                type                = tokens[0],
+                areaType            = tokens[1],
+                occupancyType       = tokens[2],
+                category            = tokens[3],
+                areaRatio           = tokens[4],
+                floorAreaRatio      = tokens[5],
+                storeys             = tokens[6],
+                version             = tokens[7]
+
+            };
+            return rainshed;
+        }
+
+        // 附属其他构件
+        public static ThAreaFrameAOccupancy Miscellaneous(string name)
+        {
+            string[] tokens = name.Split('_');
+            ThAreaFrameAOccupancy miscellaneous = new ThAreaFrameAOccupancy(name)
+            {
+                type                = tokens[0],
+                areaType            = tokens[1],
+                component           = tokens[2],
+                occupancyType       = tokens[3],
+                category            = tokens[4],
+                areaRatio           = tokens[5],
+                floorAreaRatio      = tokens[6],
+                storeys             = tokens[7],
+                version             = tokens[8]
+
+            };
+            return miscellaneous;
+        }
+    }
+}
