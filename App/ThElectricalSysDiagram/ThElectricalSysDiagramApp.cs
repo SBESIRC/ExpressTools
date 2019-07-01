61e08e0f4e79f71ec5d45429e8f0bee9b0c6f6c2
Bruce Li <384196936@qq.com>
2019-07-01 15:17:39 +0800

    Add time validate.

--- App/ThElectricalSysDiagram/ThElectricalSysDiagramApp.cs
+++ App/ThElectricalSysDiagram/ThElectricalSysDiagramApp.cs
@@ -27,6 +27,14 @@ namespace ThElectricalSysDiagram
         [CommandMethod("TIANHUACAD","THBEE", CommandFlags.Modal)]
         public void BlockExchangeCmd()
         {
+            //将程序有效期验证为3个月，一旦超过时限，要求用户更新，不进行命令注册
+            var usualDate = new DateTime(2019, 7, 1);
+            var dateTime = DateTime.Today;
+            if ((dateTime - usualDate).Days > 62)
+            {
+                return;
+            }
+
             var view = new ThSysDiagramView();
             AcadApp.ShowModalWindow(view);
         }
