d72aba177486b6da96f144b2d7f3cb9e1a9860d4
Bruce Li <384196936@qq.com>
2019-07-04 11:05:48 +0800

    Add getobjectid function with transtion parameter.

--- App/Utilities/ObjectIDTool.cs
+++ App/Utilities/ObjectIDTool.cs
@@ -8,12 +8,22 @@ namespace TianHua.AutoCAD.Utility.ExtensionTools
 {
     public static class ObjectIDTool
     {
+        public static T GetObjectByID<T>(this ObjectId id, OpenCloseTransaction tr) where T : DBObject
+        {
+            return tr.GetObject(id, OpenMode.ForRead) as T;
+        }
+
+        public static T GetObjectByID<T>(this ObjectId id, OpenCloseTransaction tr,OpenMode openMode) where T : DBObject
+        {
+            return tr.GetObject(id, openMode) as T;
+        }
+
         public static T GetObjectByID<T>(this ObjectId id) where T : DBObject
         {
             return id.GetObject(OpenMode.ForWrite) as T;
         }
 
-        public static T GetObjectByID<T>(this ObjectId id,OpenMode openMode) where T : DBObject
+        public static T GetObjectByID<T>(this ObjectId id, OpenMode openMode) where T : DBObject
         {
             return id.GetObject(openMode) as T;
         }
