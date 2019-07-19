8c73476b62ad02adf1c79468b30f240c20270387
Jerry Dong <shichongdong80@163.com>
2019-07-19 15:39:27 +0800

    Reference to Microsoft.Xaml.Behaviors.Wpf for ThParking As a result, ThParking is updated to target .NET 4.5 framework

--- App/ThParking/ThEventCommand.cs
+++ App/ThParking/ThEventCommand.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Input;
-using System.Windows.Interactivity;
+using Microsoft.Xaml.Behaviors;
 
 namespace TianHua.AutoCAD.Parking
 {
