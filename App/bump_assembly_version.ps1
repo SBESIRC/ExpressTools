c1a08d22d5a82c2a1d7e10311beaed5fb5e990a8
Jerry Dong <shichongdong80@163.com>
2019-07-25 13:43:23 +0800

    Fix some typo in run_build scripts

--- App/bump_assembly_version.ps1
+++ App/bump_assembly_version.ps1
@@ -9,62 +9,62 @@ $commond_parameter_error = 'assembly_version must like "x.x.x"'
 
 if($assembly_version_new -eq $null)
 {
-	echo $commond_parameter_missing
-	return
+    echo $commond_parameter_missing
+    return
 }
 elseif($assembly_version_old -eq $null)
 {
-	echo $commond_parameter_missing
-	return
+    echo $commond_parameter_missing
+    return
 }
 elseif($assembly_version_old -eq "ALL")
 {
-	if(!($assembly_version_new -match $version_format))
-	{
-		echo $commond_parameter_error
-		return
-	}
-	else
-	{
-		$fileList = Get-ChildItem $folder_start -recurse *$AssemblyInfo_file_name | %{$_.FullName}
-		$count = 0
-		Foreach($file in $fileList)
-		{
-			if(select-string $version_format $file)
-			{
-				(type $file) -replace ($version_format, $assembly_version_new)|out-file $file -encoding utf8
-				echo "edit ($file) finished"
-				$count++
-			}
-		}
-		echo "complete $count file"
-	}
+    if(!($assembly_version_new -match $version_format))
+    {
+        echo $commond_parameter_error
+        return
+    }
+    else
+    {
+        $fileList = Get-ChildItem $folder_start -recurse *$AssemblyInfo_file_name | %{$_.FullName}
+        $count = 0
+        Foreach($file in $fileList)
+        {
+            if(select-string $version_format $file)
+            {
+                (type $file) -replace ($version_format, $assembly_version_new)|out-file $file -encoding utf8
+                echo "edit ($file) finished"
+                $count++
+            }
+        }
+        echo "complete $count file"
+    }
 }
 else
 {
-	if(!($assembly_version_new -match $version_format))
-	{
-		echo $commond_parameter_error
-		return
-	}
-	elseif(!($assembly_version_old -match $version_format))
-	{
-		echo $commond_parameter_error
-		return
-	}
-	else
-	{
-		$fileList = Get-ChildItem $folder_start -recurse *$AssemblyInfo_file_name | %{$_.FullName}
-		$count = 0
-		Foreach($file in $fileList)
-		{
-		if(select-string $assembly_version_old $file)
-			{
-				(type $file) -replace ($assembly_version_old,$assembly_version_new)|out-file $file -encoding utf8
-				echo "edit ($file) finished"
-				$count++
-			}
-		}
-		echo "complete $count file"
-	}
+    if(!($assembly_version_new -match $version_format))
+    {
+        echo $commond_parameter_error
+        return
+    }
+    elseif(!($assembly_version_old -match $version_format))
+    {
+        echo $commond_parameter_error
+        return
+    }
+    else
+    {
+        $fileList = Get-ChildItem $folder_start -recurse *$AssemblyInfo_file_name | %{$_.FullName}
+        $count = 0
+        Foreach($file in $fileList)
+        {
+        if(select-string $assembly_version_old $file)
+            {
+                (type $file) -replace ($assembly_version_old,$assembly_version_new)|out-file $file -encoding utf8
+                echo "edit ($file) finished"
+                $count++
+            }
+        }
+        echo "complete $count file"
+    }
 }
--- App/run-build.ps1
+++ App/run-build.ps1
@@ -1,19 +1,17 @@
 ﻿param($buildType, $assembly_version)
 
-
 if($buildType -ne "Release")
 {
-	if($buildType -ne "Debug")
-	{
-		echo "please check your command about ($buildType)"
-		return
-	}
+    if($buildType -ne "Debug")
+    {
+        echo "please check your command about ($buildType)"
+        return
+    }
 }
 
 if($assembly_version -ne $null)
 {
-	.\bump_assembly_version.ps1 ".\" "ALL" $assembly_version
+    .\bump_assembly_version.ps1 ".\" "ALL" $assembly_version
 }
 
-Import-Module D:\psake-master\psake-master\src\psake.psm1
 Invoke-psake .\build.ps1 -Task "${buildType}.Build", Compile.Installer
