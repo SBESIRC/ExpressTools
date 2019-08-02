b717829d241c77640dd9421458e744359d73b21e
wulileo <1501450743@qq.com>
2019-08-02 09:38:28 +0800

    Automaticly git clone and reset commitid by tag version

---
+++ App/gitpull.ps1
@@ -0,0 +1,49 @@
+﻿param($script:gitlocation, $script:version)
+
+$commond_parameter_missing = "please ckeck your commond and use commond like:<.\shellname> <https://github.com/*> <*.*.*>"
+
+if($gitlocation -eq $null)
+{
+    Write-Host $commond_parameter_missing
+    return
+}
+elseif($gitlocation -notlike "https`:`/`/github`.com`/*")
+{
+    Write-Host "git location wrong"
+    return
+}
+if($version -eq $null)
+{
+    Write-Host $commond_parameter_missing
+    return
+}
+elseif(-not ($version -match "([0-9]{1,}\.[0-9]{1,}\.[0-9]{1,}\.[0-9]{1,}|[0-9]{1,}\.[0-9]{1,}\.[0-9]{1,})"))
+{
+    Write-Host "version wrong"
+    return
+}
+
+#git clone
+git clone $gitlocation
+
+#cd file
+$file = Get-ChildItem .\ | ?{$_.psiscontainer -eq $true} | %{$_.FullName}
+cd $file
+
+#git pull first
+git pull
+
+#git reset by version
+$taglog = git show "v$version"
+for ($i=0; $i -le $taglog.length; $i++)
+{
+    if($taglog[$i] -like 'commit *')　
+    {
+        $commitid = $taglog[$i].Substring(7, 6)
+        break
+    }
+}
+git reset --hard $commitid
+
+#cd back
+cd ../
