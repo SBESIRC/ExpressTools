42a79bf7e3d0f9facf393b6725750f57008a7ddb
wulileo <1501450743@qq.com>
2019-08-02 13:57:16 +0800

    Integrate ThHarness project

---
+++ App/ThHarness/SampleClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MbUnit.Framework;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.DatabaseServices;
+using AcadApp = Autodesk.AutoCAD.ApplicationServices.Application;
+
+namespace ThHarness
+{
+    public class SampleClass
+    {
+        [Test]
+        public static void ChangeCircleColors()
+        {
+            var document = AcadApp.DocumentManager.MdiActiveDocument;
+            var database = document.Database;
+
+            using (var tr = database.TransactionManager.StartOpenCloseTransaction())
+            {
+                try
+                {
+                    var circleClass = RXObject.GetClass(typeof(Circle));
+                    var modelSpaceId = SymbolUtilityServices.GetBlockModelSpaceId(database);
+                    var modelSpace = (BlockTableRecord)tr.GetObject(modelSpaceId, OpenMode.ForRead);
+
+                    foreach (ObjectId id in modelSpace)
+                    {
+                        if (!id.ObjectClass.IsDerivedFrom(circleClass))
+                            continue;
+
+                        var circle = (Circle)tr.GetObject(id, OpenMode.ForWrite);
+                        circle.ColorIndex = circle.Radius < 1.0 ? 2
+                                          : circle.Radius > 10.0 ? 1
+                                          : 3;
+                    }
+                    tr.Commit();
+                }
+                catch (Autodesk.AutoCAD.Runtime.Exception ex)
+                {
+                    tr.Abort();
+                    document.Editor.WriteMessage(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ App/run-harness.ps1
@@ -0,0 +1,27 @@
+﻿param($dllfile)
+
+for ($i = $dllfile.length; $i -gt 0; $i--)
+{
+    if($dllfile[$i] -eq "\")
+    {
+        break
+    }
+}
+
+$script:dllname = $dllfile.Substring($i + 1)
+$script:galliofolder = "C:\Users\$env:UserName\.nuget\packages\galliobundle\3.4.14\bin"
+
+COPY-item -r $dllfile $galliofolder
+SET-Variable GALLIO_RUNTIME_PATH=C:\Users\$env:UserName\.nuget\packages\galliobundle\3.4.14\bin
+$script:Gallioexe = resolve-path "${galliofolder}\Gallio.Echo.exe"
+if ($Gallioexe -eq $null)
+{
+    Write-Host "Failed to find Gallio.Echo.exe"
+    return
+}
+Write-Host "Found Gallio.Echo.exe here: $Gallioexe"
+
+& $Gallioexe C:\Users\$env:UserName\.nuget\packages\galliobundle\3.4.14\bin\$dllname /r:AutoCAD
+
+#Unable to delete dll file, because CAD is using it
+#Remove-Item "C:\Users\$env:UserName\.nuget\packages\galliobundle\3.4.14\bin\${dllname}"
