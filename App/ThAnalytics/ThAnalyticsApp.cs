943dbc2e37a199f85ac0eaeb3f721b87b80dd99c
wulileo <1501450743@qq.com>
2019-08-16 17:04:32 +0800

    Run Analytics at AutoCAD startup

--- App/ThAnalytics/ThAnalyticsApp.cs
+++ App/ThAnalytics/ThAnalyticsApp.cs
@@ -11,14 +11,120 @@ namespace ThAnalytics
 {
     public class ThAnalyticsApp : IExtensionApplication
     {
+        // command name filters
+        private readonly ArrayList filters = new ArrayList { "THDAS", "THDAE", "QUIT" };
+
+        private readonly Hashtable commandhashtable = new Hashtable();
+
         public void Initialize()
         {
             ThCountlyServices.Instance.Initialize();
+            AcadApp.Idle += new EventHandler(Application_OnIdle);
         }
 
         public void Terminate()
         {
+            // unhook event handlers
+            RemoveCommandHandler();
+
+            //end the user session
+            ThCountlyServices.Instance.EndSession();
         }
+
+        private void Application_OnIdle(object sender, EventArgs e)
+        {
+            AcadApp.Idle -= new EventHandler(Application_OnIdle);
+            
+            // hook event handlers
+            AddCommandHandler();
+
+            //start the user session
+            ThCountlyServices.Instance.StartSession();
+        }
+
+        private void AddCommandHandler()
+        {
+            AcadApp.DocumentManager.DocumentCreated += DocumentManager_DocumentCreated;
+
+            foreach (Document d in AcadApp.DocumentManager)
+            {
+                SubscribeToDoc(d);
+            }
+        }
+
+        private void RemoveCommandHandler()
+        {
+            AcadApp.DocumentManager.DocumentCreated -= DocumentManager_DocumentCreated;
+
+            foreach (Document d in AcadApp.DocumentManager)
+            {
+                UnsubscribeToDoc(d);
+            }
+        }
+
+        private void SubscribeToDoc(Document d)
+        {
+            d.CommandWillStart += Document_CommandWillStart;
+            d.CommandEnded += Document_CommandEnded;
+            d.CommandCancelled += Document_CommandCancelled;
+            d.CommandFailed += Documet_CommandFailed;
+            d.UnknownCommand += Document_UnknownCommand;
+        }
+
+        private void UnsubscribeToDoc(Document d)
+        {
+            d.CommandWillStart -= Document_CommandWillStart;
+            d.CommandEnded -= Document_CommandEnded;
+            d.CommandCancelled -= Document_CommandCancelled;
+            d.CommandFailed -= Documet_CommandFailed;
+            d.UnknownCommand -= Document_UnknownCommand;
+        }
+
+        private void DocumentManager_DocumentCreated(object sender, DocumentCollectionEventArgs e)
+        {
+            SubscribeToDoc(e.Document);
+        }
+
+        private void Document_CommandWillStart(object sender, CommandEventArgs e)
+        {
+            int commamndstarttime = (int)(DateTime.Now.Ticks / 10000000);
+            commandhashtable.Add(e.GlobalCommandName, commamndstarttime);
+        }
+
+        private void Document_CommandEnded(object sender, CommandEventArgs e)
+        {
+            if (filters.Contains(e.GlobalCommandName))
+                return;
+
+            if (commandhashtable.ContainsKey(e.GlobalCommandName))
+            {
+                int commamndstoptime = (int)(DateTime.Now.Ticks / 10000000);
+                int runtime = commamndstoptime - (int)commandhashtable[e.GlobalCommandName];
+                ThCountlyServices.Instance.RecordCommandEvent(e.GlobalCommandName, runtime);
+                commandhashtable.Remove(e.GlobalCommandName);
+            }
+        }
+
+        private void Document_CommandCancelled(object sender, CommandEventArgs e)
+        {
+            if (commandhashtable.ContainsKey(e.GlobalCommandName))
+            {
+                commandhashtable.Remove(e.GlobalCommandName);
+            }
+        }
+
+        private void Documet_CommandFailed(object sender, CommandEventArgs e)
+        {
+            if (commandhashtable.ContainsKey(e.GlobalCommandName))
+            {
+                commandhashtable.Remove(e.GlobalCommandName);
+            }
+        }
+
+        private void Document_UnknownCommand(object sender, UnknownCommandEventArgs e)
+        {
+        }
+
     }
 
     public class ThAnalyticsCommands
--- App/run-build.ps1
+++ App/run-build.ps1
@@ -34,6 +34,8 @@ Task Compile.Assembly.R18 -Depends Requires.MSBuild {
             & $msbuildExe /verbosity:minimal /property:OutDir=..\build\bin\$buildType\,IntermediateOutputPath=..\build\obj\$buildType\ ".\ThElectricalSysDiagram\ThElectricalSysDiagram.csproj" /p:Configuration=$buildType /t:rebuild
             & $msbuildExe /verbosity:minimal /property:OutDir=..\build\bin\$buildType\,IntermediateOutputPath=..\build\obj\$buildType\ ".\ThAnalytics\ThAnalytics.csproj" /p:Configuration=$buildType /t:restore
             & $msbuildExe /verbosity:minimal /property:OutDir=..\build\bin\$buildType\,IntermediateOutputPath=..\build\obj\$buildType\ ".\ThAnalytics\ThAnalytics.csproj" /p:Configuration=$buildType /t:rebuild
+            & $msbuildExe /verbosity:minimal /property:OutDir=..\build\bin\$buildType\,IntermediateOutputPath=..\build\obj\$buildType\ ".\ThIdentity\ThIdentity.csproj" /p:Configuration=$buildType /t:restore
+            & $msbuildExe /verbosity:minimal /property:OutDir=..\build\bin\$buildType\,IntermediateOutputPath=..\build\obj\$buildType\ ".\ThIdentity\ThIdentity.csproj" /p:Configuration=$buildType /t:rebuild
             & $msbuildExe /verbosity:minimal /property:OutDir=..\build\bin\$buildType\,IntermediateOutputPath=..\build\obj\$buildType\ ".\ThAutoUpdate\ThAutoUpdate.csproj" /p:Configuration=$buildType /t:restore
             & $msbuildExe /verbosity:minimal /property:OutDir=..\build\bin\$buildType\,IntermediateOutputPath=..\build\obj\$buildType\ ".\ThAutoUpdate\ThAutoUpdate.csproj" /p:Configuration=$buildType /t:rebuild
             & $msbuildExe /verbosity:minimal /property:OutDir=..\build\bin\$buildType\,IntermediateOutputPath=..\build\obj\$buildType\ ".\ThElectrical\ThElectrical.csproj" /p:Configuration=$buildType /t:restore
@@ -54,6 +56,8 @@ Task Compile.Assembly.R19 -Depends Requires.MSBuild {
         & $msbuildExe /verbosity:minimal /property:OutDir="..\build\bin\${buildType}-NET40\",IntermediateOutputPath="..\build\obj\${buildType}-NET40\" ".\ThElectricalSysDiagram\ThElectricalSysDiagram.csproj" /p:Configuration="${buildType}-NET40" /t:rebuild
         & $msbuildExe /verbosity:minimal /property:OutDir="..\build\bin\${buildType}-NET40\",IntermediateOutputPath="..\build\obj\${buildType}-NET40\" ".\ThAnalytics\ThAnalytics.csproj" /p:Configuration="${buildType}-NET40" /t:restore
         & $msbuildExe /verbosity:minimal /property:OutDir="..\build\bin\${buildType}-NET40\",IntermediateOutputPath="..\build\obj\${buildType}-NET40\" ".\ThAnalytics\ThAnalytics.csproj" /p:Configuration="${buildType}-NET40" /t:rebuild
+        & $msbuildExe /verbosity:minimal /property:OutDir=..\build\bin\$buildType\,IntermediateOutputPath=..\build\obj\$buildType\ ".\ThIdentity\ThIdentity.csproj" /p:Configuration=$buildType /t:restore
+        & $msbuildExe /verbosity:minimal /property:OutDir=..\build\bin\$buildType\,IntermediateOutputPath=..\build\obj\$buildType\ ".\ThIdentity\ThIdentity.csproj" /p:Configuration=$buildType /t:rebuild
         & $msbuildExe /verbosity:minimal /property:OutDir="..\build\bin\${buildType}-NET40\",IntermediateOutputPath="..\build\obj\${buildType}-NET40\" ".\ThAutoUpdate\ThAutoUpdate.csproj" /p:Configuration="${buildType}-NET40" /t:restore
         & $msbuildExe /verbosity:minimal /property:OutDir="..\build\bin\${buildType}-NET40\",IntermediateOutputPath="..\build\obj\${buildType}-NET40\" ".\ThAutoUpdate\ThAutoUpdate.csproj" /p:Configuration="${buildType}-NET40" /t:rebuild
         & $msbuildExe /verbosity:minimal /property:OutDir="..\build\bin\${buildType}-NET40\",IntermediateOutputPath="..\build\obj\${buildType}-NET40\" ".\ThElectrical\ThElectrical.csproj" /p:Configuration="${buildType}-NET40" /t:restore
@@ -74,6 +78,8 @@ Task Compile.Assembly.R20 -Depends Requires.MSBuild {
         & $msbuildExe /verbosity:minimal /property:OutDir="..\build\bin\${buildType}-NET45\",IntermediateOutputPath="..\build\obj\${buildType}-NET45\" ".\ThElectricalSysDiagram\ThElectricalSysDiagram.csproj" "${buildType}-NET45" /t:rebuild
         & $msbuildExe /verbosity:minimal /property:OutDir="..\build\bin\${buildType}-NET45\",IntermediateOutputPath="..\build\obj\${buildType}-NET45\" ".\ThAnalytics\ThAnalytics.csproj" "${buildType}-NET45" /t:restore
         & $msbuildExe /verbosity:minimal /property:OutDir="..\build\bin\${buildType}-NET45\",IntermediateOutputPath="..\build\obj\${buildType}-NET45\" ".\ThAnalytics\ThAnalytics.csproj" "${buildType}-NET45" /t:rebuild
+		& $msbuildExe /verbosity:minimal /property:OutDir=..\build\bin\$buildType\,IntermediateOutputPath=..\build\obj\$buildType\ ".\ThIdentity\ThIdentity.csproj" /p:Configuration=$buildType /t:restore
+        & $msbuildExe /verbosity:minimal /property:OutDir=..\build\bin\$buildType\,IntermediateOutputPath=..\build\obj\$buildType\ ".\ThIdentity\ThIdentity.csproj" /p:Configuration=$buildType /t:rebuild
         & $msbuildExe /verbosity:minimal /property:OutDir="..\build\bin\${buildType}-NET45\",IntermediateOutputPath="..\build\obj\${buildType}-NET45\" ".\ThElectrical\ThElectrical.csproj" "${buildType}-NET45" /t:restore
         & $msbuildExe /verbosity:minimal /property:OutDir="..\build\bin\${buildType}-NET45\",IntermediateOutputPath="..\build\obj\${buildType}-NET45\" ".\ThElectrical\ThElectrical.csproj" "${buildType}-NET45" /t:rebuild
     }
@@ -97,6 +103,7 @@ Task Compile.Installer -Depends Requires.BuildType, Compile.Assembly.R18, Compil
         & $msbuildExe /verbosity:minimal ".\ThParking\ThParking.csproj" /p:Configuration=$buildType /t:restore
         & $msbuildExe /verbosity:minimal ".\ThAreaFrame\ThAreaFrame.csproj" /p:Configuration=$buildType /t:restore
         & $msbuildExe /verbosity:minimal ".\ThAnalytics\ThAnalytics.csproj" /p:Configuration=Release /t:restore
+        & $msbuildExe /verbosity:minimal ".\ThIdentity\ThIdentity.csproj" /p:Configuration=Release /t:restore
         & $msbuildExe /verbosity:minimal ".\ThAutoUpdate\ThAutoUpdate.csproj" /p:Configuration=Release /t:restore
         & $msbuildExe /verbosity:minimal ".\ThElectrical\ThElectrical.csproj" /p:Configuration=Release /t:restore
         & $msbuildExe /verbosity:minimal ".\ThElectricalSysDiagram\ThElectricalSysDiagram.csproj" /p:Configuration=$buildType /t:restore
