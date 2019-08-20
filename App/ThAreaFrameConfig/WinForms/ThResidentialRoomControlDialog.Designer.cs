7b5125064c3e73058dc2f9006f42225f943c0c76
Jerry Dong <shichongdong80@163.com>
2019-08-20 16:25:19 +0800

    ThResidentialRoomControlDialog size

--- App/ThAreaFrameConfig/WinForms/ThResidentialRoomControlDialog.Designer.cs
+++ App/ThAreaFrameConfig/WinForms/ThResidentialRoomControlDialog.Designer.cs
@@ -28,49 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.tabPane1 = new DevExpress.XtraBars.Navigation.TabPane();
             this.tabNavigationPage1 = new DevExpress.XtraBars.Navigation.TabNavigationPage();
-            this.thResidentialBuildingControl1 = new ThAreaFrameConfig.WinForms.ThResidentialBuildingControl();
             this.tabNavigationPage2 = new DevExpress.XtraBars.Navigation.TabNavigationPage();
-            this.thResidentialRoomControl1 = new ThAreaFrameConfig.WinForms.ThResidentialRoomControl();
             this.tabNavigationPage3 = new DevExpress.XtraBars.Navigation.TabNavigationPage();
             this.tabNavigationPage4 = new DevExpress.XtraBars.Navigation.TabNavigationPage();
-            this.thAOccupancyControl1 = new ThAreaFrameConfig.WinForms.ThAOccupancyControl();
             this.tabNavigationPage5 = new DevExpress.XtraBars.Navigation.TabNavigationPage();
-            this.thRoofGreenSpaceControl1 = new ThAreaFrameConfig.WinForms.ThRoofGreenSpaceControl();
             this.tabNavigationPage6 = new DevExpress.XtraBars.Navigation.TabNavigationPage();
-            this.thUnderGroundParkingControl1 = new ThAreaFrameConfig.WinForms.ThUnderGroundParkingControl();
             this.tabNavigationPage7 = new DevExpress.XtraBars.Navigation.TabNavigationPage();
+            this.thResidentialBuildingControl1 = new ThAreaFrameConfig.WinForms.ThResidentialBuildingControl();
+            this.thResidentialRoomControl1 = new ThAreaFrameConfig.WinForms.ThResidentialRoomControl();
+            this.thAOccupancyControl1 = new ThAreaFrameConfig.WinForms.ThAOccupancyControl();
+            this.thRoofGreenSpaceControl1 = new ThAreaFrameConfig.WinForms.ThRoofGreenSpaceControl();
+            this.thUnderGroundParkingControl1 = new ThAreaFrameConfig.WinForms.ThUnderGroundParkingControl();
             this.thRoofControl1 = new ThAreaFrameConfig.WinForms.ThRoofControl();
             ((System.ComponentModel.ISupportInitialize)(this.tabPane1)).BeginInit();
             this.tabPane1.SuspendLayout();
             this.tabNavigationPage1.SuspendLayout();
             this.tabNavigationPage2.SuspendLayout();
             this.tabNavigationPage3.SuspendLayout();
+            this.tabNavigationPage4.SuspendLayout();
             this.tabNavigationPage5.SuspendLayout();
             this.tabNavigationPage6.SuspendLayout();
             this.tabNavigationPage7.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(93, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // tabPane1
             // 
@@ -81,7 +62,7 @@
             this.tabPane1.Controls.Add(this.tabNavigationPage5);
             this.tabPane1.Controls.Add(this.tabNavigationPage6);
             this.tabPane1.Controls.Add(this.tabNavigationPage7);
-            this.tabPane1.Location = new System.Drawing.Point(12, 41);
+            this.tabPane1.Location = new System.Drawing.Point(12, 12);
             this.tabPane1.Name = "tabPane1";
             this.tabPane1.Pages.AddRange(new DevExpress.XtraBars.Navigation.NavigationPageBase[] {
             this.tabNavigationPage1,
@@ -91,9 +72,9 @@
             this.tabNavigationPage5,
             this.tabNavigationPage6,
             this.tabNavigationPage7});
-            this.tabPane1.RegularSize = new System.Drawing.Size(860, 547);
+            this.tabPane1.RegularSize = new System.Drawing.Size(501, 354);
             this.tabPane1.SelectedPage = this.tabNavigationPage1;
-            this.tabPane1.Size = new System.Drawing.Size(860, 547);
+            this.tabPane1.Size = new System.Drawing.Size(501, 354);
             this.tabPane1.TabIndex = 2;
             this.tabPane1.Text = "tabPane1";
             // 
@@ -102,106 +83,105 @@
             this.tabNavigationPage1.Caption = "单体信息";
             this.tabNavigationPage1.Controls.Add(this.thResidentialBuildingControl1);
             this.tabNavigationPage1.Name = "tabNavigationPage1";
-            this.tabNavigationPage1.Size = new System.Drawing.Size(842, 501);
-            // 
-            // thResidentialBuildingControl1
-            // 
-            this.thResidentialBuildingControl1.AutoSize = true;
-            this.thResidentialBuildingControl1.Location = new System.Drawing.Point(3, 3);
-            this.thResidentialBuildingControl1.Name = "thResidentialBuildingControl1";
-            this.thResidentialBuildingControl1.Size = new System.Drawing.Size(836, 495);
-            this.thResidentialBuildingControl1.TabIndex = 0;
+            this.tabNavigationPage1.Size = new System.Drawing.Size(483, 308);
             // 
             // tabNavigationPage2
             // 
             this.tabNavigationPage2.Caption = "住宅户型";
             this.tabNavigationPage2.Controls.Add(this.thResidentialRoomControl1);
             this.tabNavigationPage2.Name = "tabNavigationPage2";
-            this.tabNavigationPage2.Size = new System.Drawing.Size(842, 501);
-            // 
-            // thResidentialRoomControl1
-            // 
-            this.thResidentialRoomControl1.AutoSize = true;
-            this.thResidentialRoomControl1.Location = new System.Drawing.Point(3, 3);
-            this.thResidentialRoomControl1.Name = "thResidentialRoomControl1";
-            this.thResidentialRoomControl1.Size = new System.Drawing.Size(834, 495);
-            this.thResidentialRoomControl1.TabIndex = 0;
+            this.tabNavigationPage2.Size = new System.Drawing.Size(483, 308);
             // 
             // tabNavigationPage3
             // 
             this.tabNavigationPage3.Caption = "附属公建";
             this.tabNavigationPage3.Controls.Add(this.thAOccupancyControl1);
             this.tabNavigationPage3.Name = "tabNavigationPage3";
-            this.tabNavigationPage3.Size = new System.Drawing.Size(758, 351);
-            // 
-            // thAOccupancyControl1
-            // 
-            this.thAOccupancyControl1.AutoSize = true;
-            this.thAOccupancyControl1.Location = new System.Drawing.Point(3, 3);
-            this.thAOccupancyControl1.Name = "thAOccupancyControl1";
-            this.thAOccupancyControl1.Size = new System.Drawing.Size(834, 495);
-            this.thAOccupancyControl1.TabIndex = 0;
+            this.tabNavigationPage3.Size = new System.Drawing.Size(483, 308);
             // 
             // tabNavigationPage4
             // 
             this.tabNavigationPage4.Caption = "公摊";
             this.tabNavigationPage4.Name = "tabNavigationPage4";
-            this.tabNavigationPage4.Size = new System.Drawing.Size(758, 351);
             this.tabNavigationPage4.PageVisible = false;
+            this.tabNavigationPage4.Size = new System.Drawing.Size(483, 308);
             // 
             // tabNavigationPage5
             // 
             this.tabNavigationPage5.Caption = "屋顶绿地";
             this.tabNavigationPage5.Controls.Add(this.thRoofGreenSpaceControl1);
             this.tabNavigationPage5.Name = "tabNavigationPage5";
-            this.tabNavigationPage5.Size = new System.Drawing.Size(758, 351);
-            // 
-            // thRoofGreenSpaceControl1
-            // 
-            this.thRoofGreenSpaceControl1.AutoSize = true;
-            this.thRoofGreenSpaceControl1.Location = new System.Drawing.Point(3, 3);
-            this.thRoofGreenSpaceControl1.Name = "thRoofGreenSpaceControl1";
-            this.thRoofGreenSpaceControl1.Size = new System.Drawing.Size(834, 495);
-            this.thRoofGreenSpaceControl1.TabIndex = 0;
+            this.tabNavigationPage5.Size = new System.Drawing.Size(483, 308);
             // 
             // tabNavigationPage6
             // 
             this.tabNavigationPage6.Caption = "室内车位";
             this.tabNavigationPage6.Controls.Add(this.thUnderGroundParkingControl1);
             this.tabNavigationPage6.Name = "tabNavigationPage6";
-            this.tabNavigationPage6.Size = new System.Drawing.Size(758, 351);
-            // 
-            // thUnderGroundParkingControl1
-            // 
-            this.thUnderGroundParkingControl1.AutoSize = true;
-            this.thUnderGroundParkingControl1.Location = new System.Drawing.Point(3, 3);
-            this.thUnderGroundParkingControl1.Name = "thUnderGroundParkingControl1";
-            this.thUnderGroundParkingControl1.Size = new System.Drawing.Size(834, 495);
-            this.thUnderGroundParkingControl1.TabIndex = 0;
+            this.tabNavigationPage6.Size = new System.Drawing.Size(483, 308);
             // 
             // tabNavigationPage7
             // 
             this.tabNavigationPage7.Caption = "楼顶间";
             this.tabNavigationPage7.Controls.Add(this.thRoofControl1);
             this.tabNavigationPage7.Name = "tabNavigationPage7";
-            this.tabNavigationPage7.Size = new System.Drawing.Size(758, 351);
+            this.tabNavigationPage7.Size = new System.Drawing.Size(483, 308);
+            // 
+            // thResidentialBuildingControl1
+            // 
+            this.thResidentialBuildingControl1.AutoSize = true;
+            this.thResidentialBuildingControl1.Location = new System.Drawing.Point(3, 3);
+            this.thResidentialBuildingControl1.Name = "thResidentialBuildingControl1";
+            this.thResidentialBuildingControl1.Size = new System.Drawing.Size(477, 302);
+            this.thResidentialBuildingControl1.TabIndex = 0;
+            // 
+            // thResidentialRoomControl1
+            // 
+            this.thResidentialRoomControl1.AutoSize = true;
+            this.thResidentialRoomControl1.Location = new System.Drawing.Point(3, 3);
+            this.thResidentialRoomControl1.Name = "thResidentialRoomControl1";
+            this.thResidentialRoomControl1.Size = new System.Drawing.Size(477, 302);
+            this.thResidentialRoomControl1.TabIndex = 0;
+            // 
+            // thAOccupancyControl1
+            // 
+            this.thAOccupancyControl1.AutoSize = true;
+            this.thAOccupancyControl1.Location = new System.Drawing.Point(3, 3);
+            this.thAOccupancyControl1.Name = "thAOccupancyControl1";
+            this.thAOccupancyControl1.Size = new System.Drawing.Size(477, 302);
+            this.thAOccupancyControl1.TabIndex = 0;
+            // 
+            // thRoofGreenSpaceControl1
+            // 
+            this.thRoofGreenSpaceControl1.AutoSize = true;
+            this.thRoofGreenSpaceControl1.Location = new System.Drawing.Point(3, 3);
+            this.thRoofGreenSpaceControl1.Name = "thRoofGreenSpaceControl1";
+            this.thRoofGreenSpaceControl1.Size = new System.Drawing.Size(477, 302);
+            this.thRoofGreenSpaceControl1.TabIndex = 0;
+            // 
+            // thUnderGroundParkingControl1
+            // 
+            this.thUnderGroundParkingControl1.AutoSize = true;
+            this.thUnderGroundParkingControl1.Location = new System.Drawing.Point(3, 3);
+            this.thUnderGroundParkingControl1.Name = "thUnderGroundParkingControl1";
+            this.thUnderGroundParkingControl1.Size = new System.Drawing.Size(477, 302);
+            this.thUnderGroundParkingControl1.TabIndex = 0;
             // 
             // thRoofControl1
             // 
             this.thRoofControl1.AutoSize = true;
             this.thRoofControl1.Location = new System.Drawing.Point(3, 3);
             this.thRoofControl1.Name = "thRoofControl1";
-            this.thRoofControl1.Size = new System.Drawing.Size(834, 495);
+            this.thRoofControl1.Roofs = null;
+            this.thRoofControl1.Size = new System.Drawing.Size(477, 302);
             this.thRoofControl1.TabIndex = 0;
             // 
             // ThResidentialRoomControlDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(884, 600);
+            this.ClientSize = new System.Drawing.Size(525, 378);
             this.Controls.Add(this.tabPane1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Name = "ThResidentialRoomControlDialog";
             this.Text = "天华单体框线图层规整";
             ((System.ComponentModel.ISupportInitialize)(this.tabPane1)).EndInit();
@@ -212,6 +192,8 @@
             this.tabNavigationPage2.PerformLayout();
             this.tabNavigationPage3.ResumeLayout(false);
             this.tabNavigationPage3.PerformLayout();
+            this.tabNavigationPage4.ResumeLayout(false);
+            this.tabNavigationPage4.PerformLayout();
             this.tabNavigationPage5.ResumeLayout(false);
             this.tabNavigationPage5.PerformLayout();
             this.tabNavigationPage6.ResumeLayout(false);
@@ -223,9 +205,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private DevExpress.XtraBars.Navigation.TabPane tabPane1;
         private DevExpress.XtraBars.Navigation.TabNavigationPage tabNavigationPage1;
         private ThResidentialBuildingControl thResidentialBuildingControl1;
