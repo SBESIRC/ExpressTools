41d4ebcb994d1d7a315c31c28359fccdae1c5b6a
wulileo <1501450743@qq.com>
2019-08-29 16:07:15 +0800

    Update SampleClass.cs

--- App/ThHarness/SampleClass.cs
+++ App/ThHarness/SampleClass.cs
@@ -12,21 +12,35 @@ namespace SampleClass
     [TestFixture]
     class ThUiTest
     {
-        
-        [Test]
-        public void Basicaltest()
+        private WindowsDriver<WindowsElement> AutoCAD;
+
+        [SetUp]
+        public void Setup()
         {
             // Launch the AutoCAD
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.SetCapability("app", @"C:\Program Files\Autodesk\AutoCAD 2012 - Simplified Chinese\acad.exe");
-            WindowsDriver<WindowsElement> AutoCAD = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), capabilities);
+            AutoCAD = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), capabilities);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            // 关闭CAD
+            AutoCAD.FindElementByName("关闭").Click();
+            // 是否保存——否
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            AutoCAD.FindElementByAccessibilityId("7").Click();
+        }
+
+        [Test]
+        public void Basicaltest()
+        {
 
             // Click or SendKeys
-            AutoCAD.Keyboard.SendKeys("STYLE\n");
-            while (AutoCAD.FindElementsByName("文字样式").Count == 0)
-            {
-                Thread.Sleep(TimeSpan.FromSeconds(1));
-            }
+            AutoCAD.FindElementByAccessibilityId("2").SendKeys("STYLE\n");
+            Thread.Sleep(TimeSpan.FromSeconds(3));
+            
             // 颠倒
             AutoCAD.FindElementByAccessibilityId("906").Click();
             // 高度
@@ -38,7 +52,7 @@ namespace SampleClass
             AutoCAD.FindElementByAccessibilityId("917").Click();
 
             // 画线
-            AutoCAD.FindElementByAccessibilityId("ID_Line").Click();
+            AutoCAD.FindElementByAccessibilityId("2").SendKeys("LINE\n");
             WindowsElement paintplate = AutoCAD.FindElementByAccessibilityId("59648");
             AutoCAD.Mouse.MouseMove(paintplate.Coordinates, 100, 100);
             AutoCAD.Mouse.Click(null);
@@ -47,40 +61,27 @@ namespace SampleClass
             AutoCAD.Mouse.MouseMove(paintplate.Coordinates, 100, 200);
             AutoCAD.Mouse.Click(null);
             AutoCAD.Keyboard.SendKeys("C\n");
-
-            // 关闭CAD
-            AutoCAD.FindElementByName("关闭").Click();
-            // 是否保存——否
-            AutoCAD.FindElementByAccessibilityId("7").Click();
         }
-        
+
         [Test]
         public void THBPStest()
         {
-            // Launch the AutoCAD
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.SetCapability("app", @"C:\Program Files\Autodesk\AutoCAD 2012 - Simplified Chinese\acad.exe");
-            WindowsDriver<WindowsElement> AutoCAD = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), capabilities);
-
             // 画布窗口
             WindowsElement paintplate = AutoCAD.FindElementByAccessibilityId("59648");
 
             // 画线
             AutoCAD.FindElementByAccessibilityId("2").SendKeys("PLINE\n");
-            AutoCAD.Mouse.MouseMove(paintplate.Coordinates, 100, 100);
+            AutoCAD.Mouse.MouseMove(paintplate.Coordinates, 300, 100);
             AutoCAD.Mouse.Click(null);
-            AutoCAD.Mouse.MouseMove(paintplate.Coordinates, 200, 100);
+            AutoCAD.Mouse.MouseMove(paintplate.Coordinates, 400, 100);
             AutoCAD.Mouse.Click(null);
-            AutoCAD.Mouse.MouseMove(paintplate.Coordinates, 100, 200);
+            AutoCAD.Mouse.MouseMove(paintplate.Coordinates, 300, 200);
             AutoCAD.Mouse.Click(null);
             AutoCAD.Keyboard.SendKeys("C\n");
 
             // Click or SendKeys
-            AutoCAD.Keyboard.SendKeys("THBPS\n");
-            while (AutoCAD.FindElementsByName("天华单体框线图层规整").Count == 0)
-            {
-                Thread.Sleep(TimeSpan.FromSeconds(10));
-            }
+            AutoCAD.FindElementByAccessibilityId("2").SendKeys("THBPS\n");
+            Thread.Sleep(TimeSpan.FromSeconds(5));
 
             // 输入值
             AutoCAD.FindElementByAccessibilityId("textEdit_number").SendKeys("1#");
@@ -90,45 +91,29 @@ namespace SampleClass
             AutoCAD.FindElementByAccessibilityId("textEdit_under_ground_storeys").SendKeys("1");
 
             AutoCAD.FindElementByAccessibilityId("simpleButton_OK").Click();
-            AutoCAD.Mouse.MouseMove(paintplate.Coordinates, 100, 100);
+            AutoCAD.Mouse.MouseMove(paintplate.Coordinates, 300, 100);
             AutoCAD.Mouse.Click(null);
             AutoCAD.Keyboard.SendKeys("\n");
 
-            // 关闭CAD
-            AutoCAD.FindElementByName("关闭").Click();
-            // 是否保存——否
-            AutoCAD.FindElementByAccessibilityId("7").Click();
+            //
+            Assert.AreEqual(AutoCAD.FindElementByAccessibilityId("textEdit_number").Text, "1#");
         }
 
         [Test]
         public void THBPPtest()
         {
-            // Launch the AutoCAD
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.SetCapability("app", @"C:\Program Files\Autodesk\AutoCAD 2012 - Simplified Chinese\acad.exe");
-            WindowsDriver<WindowsElement> AutoCAD = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), capabilities);
-
             // 画布窗口
             WindowsElement paintplate = AutoCAD.FindElementByAccessibilityId("59648");
 
             // 打开图纸
-
-
+            
             // Click or SendKeys
-            AutoCAD.Keyboard.SendKeys("THBPP\n");
-            while (AutoCAD.FindElementsByName("批量打印到PPT").Count == 0)
-            {
-                Thread.Sleep(TimeSpan.FromSeconds(10));
-            }
-
+            AutoCAD.FindElementByAccessibilityId("2").SendKeys("THBPP\n");
+            Thread.Sleep(TimeSpan.FromSeconds(3));
             // 输入值
 
             // 确定
             AutoCAD.FindElementByAccessibilityId("btnOK").Click();
-            // 关闭CAD
-            AutoCAD.FindElementByName("关闭").Click();
-            // 是否保存——否
-            AutoCAD.FindElementByAccessibilityId("7").Click();
         }
     }
 }
