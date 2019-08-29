39b492bfc11a1bcc234799f94c29f6615f39a654
Jerry Dong <shichongdong80@163.com>
2019-08-29 18:18:59 +0800

    Fix CS0067 C# compiler warning

--- App/ThCui/RibbonCommandHandler.cs
+++ App/ThCui/RibbonCommandHandler.cs
@@ -1,69 +1,33 @@
-﻿using Autodesk.AutoCAD.ApplicationServices;
+﻿using System;
 using Autodesk.Windows;
-using System;
-using System.Runtime.InteropServices;
-using System.Windows;
-using System.Windows.Input;
+using Autodesk.AutoCAD.ApplicationServices;
 using AcadApp = Autodesk.AutoCAD.ApplicationServices.Application;
 
 namespace TianHua.AutoCAD.ThCui
 {
     public class RibbonCommandHandler : System.Windows.Input.ICommand
     {
-        /// <summary>
-        /// 判断命令是否可以执行的方法
-        /// </summary>
-        private Func<object, bool> _canExecute;
         public bool CanExecute(object parameter)
         {
-            if (_canExecute == null) return true;
-            return _canExecute(parameter);
-            //return true;//确定此命令可以在其当前状态下执行
+            return true;
         }
-        //当出现影响是否应执行该命令的更改时发生
-        public event EventHandler CanExecuteChanged
-        {
-            add
-            {
-                if (_canExecute != null)
-                {
-                    CommandManager.RequerySuggested += value;
-                }
-            }
-            remove
-            {
-                if (_canExecute != null)
-                {
-                    CommandManager.RequerySuggested -= value;
-                }
-            }
-        }
+
+        // https://blogs.msdn.microsoft.com/trevor/2008/08/14/c-warning-cs0067-the-event-event-is-never-used/
+        public event EventHandler CanExecuteChanged = delegate { };
 
         public void Execute(object parameter)
         {
-            //获取发出命令的按钮对象
-            RibbonButton button = parameter as RibbonButton;
-            //如果发出命令的不是按钮或按钮未定义命令参数，则返回
-            if (button == null || button.CommandParameter == null) return;
+            if (parameter is RibbonCommandItem ribbonItem)
+            {
+                Document dwg = AcadApp.DocumentManager.MdiActiveDocument;
+                string cmdString = (ribbonItem.CommandParameter as string).TrimEnd();
+                if (!cmdString.EndsWith(";"))
+                {
+                    cmdString = cmdString + " ";
+                }
 
-            //根据按钮的命令参数，执行对应的AutoCAD命令
-            Document doc = AcadApp.DocumentManager.MdiActiveDocument;
-
-            #region 正确匹配esc次数，但不能捕获lsp的command，先放着
-            ////执行之前，先根据实际运行情况，决定按esc的次数
-            //string esc = "";
-            //string cmds = (string)AcadApp.GetSystemVariable("CMDNAMES");
-            //if (cmds.Length > 0)
-            //{
-            //    int cmdNum = cmds.Split(new char[] { '\'' }).Length;
-            //    for (int i = 0; i < cmdNum; i++)
-            //        esc += '\x03';
-            //} 
-            #endregion
-            //执行命令，先退出所有当前命令，再执行
-            doc.SendStringToExecute("\x03\x03" + button.CommandParameter.ToString()+" ", true, false, true);
-
+                dwg.SendStringToExecute("\x03\x03" + cmdString, true, false, true);
+            }
         }
-
     }
 }
