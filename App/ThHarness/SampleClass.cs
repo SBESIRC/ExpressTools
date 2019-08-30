d8cffd93efd92dd91654b8c44f441cc058aa032f
wulileo <1501450743@qq.com>
2019-08-30 14:07:38 +0800

    Update SampleClass.cs

--- App/ThHarness/SampleClass.cs
+++ App/ThHarness/SampleClass.cs
@@ -21,6 +21,7 @@ namespace SampleClass
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.SetCapability("app", @"C:\Program Files\Autodesk\AutoCAD 2012 - Simplified Chinese\acad.exe");
             AutoCAD = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), capabilities);
+            AutoCAD.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
         }
 
         [TearDown]
@@ -32,9 +33,9 @@ namespace SampleClass
             Thread.Sleep(TimeSpan.FromSeconds(1));
             AutoCAD.FindElementByAccessibilityId("7").Click();
         }
-
+        
         [Test]
-        public void Basicaltest()
+        public void Test_a_Basicaltest()
         {
 
             // Click or SendKeys
@@ -64,7 +65,7 @@ namespace SampleClass
         }
 
         [Test]
-        public void THBPStest()
+        public void Test_b_THBPStest()
         {
             // 画布窗口
             WindowsElement paintplate = AutoCAD.FindElementByAccessibilityId("59648");
@@ -101,30 +102,61 @@ namespace SampleClass
             //
             Assert.AreEqual(AutoCAD.FindElementByAccessibilityId("comboBoxEdit_category").Text, "公建");
         }
-
+        
         [Test]
-        public void THBPPtest()
+        public void Test_c_THBPPtest()
         {
             // 画布窗口
             WindowsElement paintplate = AutoCAD.FindElementByAccessibilityId("59648");
 
             // 打开图纸
-            // 画线
-            AutoCAD.FindElementByAccessibilityId("2").SendKeys("LINE\n");
-            AutoCAD.Mouse.MouseMove(paintplate.Coordinates, 100, 100);
-            AutoCAD.Mouse.Click(null);
-            AutoCAD.Mouse.MouseMove(paintplate.Coordinates, 200, 200);
-            AutoCAD.Mouse.Click(null);
-            AutoCAD.Mouse.MouseMove(paintplate.Coordinates, 100, 200);
-            AutoCAD.Mouse.Click(null);
-            AutoCAD.Keyboard.SendKeys("C\n");
-
+            AutoCAD.FindElementByAccessibilityId("2").SendKeys("OPEN\n");
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            AutoCAD.Mouse.MouseMove(AutoCAD.FindElementByName("打印到PDF测试图纸").Coordinates);
+            AutoCAD.Mouse.DoubleClick(null);
+            // SHX选择
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            if (AutoCAD.FindElementsByName("缺少 SHX 文件").Count != 0)
+            {
+                AutoCAD.Mouse.MouseMove(AutoCAD.FindElementByName("忽略缺少的 SHX 文件并继续").Coordinates);
+                AutoCAD.Mouse.Click(null);
+            }
+            // 代理图形选择
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            if (AutoCAD.FindElementsByName("代理信息").Count != 0)
+            {
+                AutoCAD.Mouse.MouseMove(AutoCAD.FindElementByName("显示代理图形(S)").Coordinates);
+                AutoCAD.Mouse.Click(null);
+                AutoCAD.FindElementByName("确定").Click();
+            }
             // Click or SendKeys
             AutoCAD.FindElementByAccessibilityId("2").SendKeys("THBPP\n");
             Thread.Sleep(TimeSpan.FromSeconds(2));
-            // 输入值
-
+            AutoCAD.FindElementByAccessibilityId("radioLeft2Right").Click();
+            // 输入值1
+            AutoCAD.FindElementByAccessibilityId("comboPPTLayer").Click();
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            AutoCAD.Mouse.MouseMove(AutoCAD.FindElementByAccessibilityId("comboPPTLayer").Coordinates, AutoCAD.FindElementByAccessibilityId("comboPPTLayer").Size.Width / 2, 230);
+            AutoCAD.Mouse.Click(null);
+            // 输入值2
+            AutoCAD.FindElementByAccessibilityId("comboPrintImage").Click();
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            AutoCAD.Mouse.MouseMove(AutoCAD.FindElementByAccessibilityId("comboPrintImage").Coordinates, AutoCAD.FindElementByAccessibilityId("comboPrintImage").Size.Width / 2, 240);
+            AutoCAD.Mouse.Click(null);
+            // 输入值3
+            AutoCAD.FindElementByAccessibilityId("comboPrintTextLayer").Click();
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            AutoCAD.Mouse.MouseMove(AutoCAD.FindElementByAccessibilityId("comboPrintTextLayer").Coordinates, AutoCAD.FindElementByAccessibilityId("comboPrintTextLayer").Size.Width / 2, 250);
+            AutoCAD.Mouse.Click(null);
+            // 选择
+            AutoCAD.FindElementByAccessibilityId("btnSelectPrintProfile").Click();
+            AutoCAD.Mouse.MouseMove(paintplate.Coordinates, 100, 100);
+            AutoCAD.Mouse.Click(null);
+            AutoCAD.Mouse.MouseMove(paintplate.Coordinates, 1800, 700);
+            AutoCAD.Mouse.Click(null);
+            AutoCAD.Keyboard.SendKeys("\n");
             // 确定
+            Thread.Sleep(TimeSpan.FromSeconds(1));
             AutoCAD.FindElementByAccessibilityId("btnOK").Click();
         }
     }
