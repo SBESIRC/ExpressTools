76c4d7eb432e02d1a188ea11c179cc6de6f4303c
coconavy <986489314@qq.com>
2019-09-05 10:38:48 +0800

    add config (#22)

---
+++ App/ThRoomBoundary/ThRoomBoundaryApp.cs
@@ -0,0 +1,29 @@
+﻿using Linq2Acad;
+using DotNetARX;
+using AcHelper;
+using System.Linq;
+using Autodesk.AutoCAD.Runtime;
+
+
+namespace ThRoomBoundary
+{
+    public class ThRoomBoundaryApp : IExtensionApplication
+    {
+        public void Initialize()
+        {
+        }
+
+        public void Terminate()
+        {
+        }
+
+        [CommandMethod("TIANHUACAD", "THABC", CommandFlags.Modal)]
+        static public void RoomBoundaryCalculate()
+        {
+            //using (AcadDatabase acadDatabase = AcadDatabase.Active())
+            //{
+            //    //acadDatabase.Layers.w
+            //}
+        }
+    }
+}
