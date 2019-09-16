2f99977f0594097de57a1dcb848ef55f3214941a
Jerry Dong <shichongdong80@163.com>
2019-09-16 23:51:28 +0800

    Implement ThCADCommon class

---
+++ App/Utilities/ThCADCommon.cs
@@ -0,0 +1,12 @@
+﻿namespace TianHua.AutoCAD.Utility.ExtensionTools
+{
+    public class ThCADCommon
+    {
+        // RegAppName
+        public static readonly string RegAppName = "THCAD";
+
+        // Area Frame
+        public static readonly string RegAppName_AreaFrame = "THCAD_AF";
+        public static readonly string RegAppName_AreaFrame_Version = "V2.2";
+    }
+}
