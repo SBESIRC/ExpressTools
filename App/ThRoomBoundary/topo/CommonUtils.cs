6f95a92154d19dd198142eaf0ce55afa6f1449e8
coconavy <986489314@qq.com>
2019-09-16 17:56:10 +0800

    add some area code (#30)

--- App/ThRoomBoundary/topo/CommonUtils.cs
+++ App/ThRoomBoundary/topo/CommonUtils.cs
@@ -75,6 +75,40 @@ namespace ThRoomBoundary.topo
                 return false;
         }
 
+        public static bool PtInLoop(List<LineSegment2d> loop, Point2d pt)
+        {
+            Point2d end = new Point2d(pt.X + 100000, pt.Y);
+            LineSegment2d intersectLine = new LineSegment2d(pt, end);
+            var ptLst = new List<Point2d>();
+
+            foreach (var edge in loop)
+            {
+                LineSegment2d line = new LineSegment2d(edge.StartPoint, edge.EndPoint);
+                var intersectPts = line.IntersectWith(intersectLine);
+                if (intersectPts != null && intersectPts.Count() == 1)
+                {
+                    var nPt = intersectPts.First();
+                    bool bInLst = false;
+                    foreach (var curpt in ptLst)
+                    {
+                        if (CommonUtils.Point2dIsEqualPoint2d(nPt, curpt))
+                        {
+                            bInLst = true;
+                            break;
+                        }
+                    }
+
+                    if (!bInLst)
+                        ptLst.Add(nPt);
+                }
+
+            }
+
+            if (ptLst.Count % 2 == 1)
+                return true;
+            else
+                return false;
+        }
         /// <summary>
         /// outerEdge是否包含innerEdge
         /// </summary>
@@ -88,6 +122,21 @@ namespace ThRoomBoundary.topo
             {
                 var pt = edge.Start;
                 if (!PtInLoop(outerEdge, pt))
+                {
+                    bIn = false;
+                    break;
+                }
+            }
+            return bIn;
+        }
+
+        public static bool OutLoopContainsInnerLoop(List<LineSegment2d> outerprofile, List<TopoEdge> innerEdge)
+        {
+            bool bIn = true;
+            foreach (var edge in innerEdge)
+            {
+                var pt = edge.Start;
+                if (!PtInLoop(outerprofile, pt))
                 {
                     bIn = false;
                     break;
@@ -113,6 +162,54 @@ namespace ThRoomBoundary.topo
             }
 
             return area;
+        }
+
+        /// <summary>
+        /// 面积计算
+        /// </summary>
+        /// <param name="loop"></param>
+        /// <returns></returns>
+        public static double CalcuLoopArea(List<LineSegment2d> loop)
+        {
+            double area = 0.0;
+
+            foreach (var edge in loop)
+            {
+                Point2d start = edge.StartPoint;
+                Point2d end = edge.EndPoint;
+                area += 0.5 * (start.X * end.Y - start.Y * end.X);
+            }
+
+            return area;
+        }
+
+        /// <summary>
+        /// 计算形心
+        /// </summary>
+        /// <param name="edges"></param>
+        /// <returns></returns>
+        public static Point2d CalculateTopoEdgePos(List<TopoEdge> edges)
+        {
+            var pt = edges.First().Start;
+            for (int j = 1; j < edges.Count; j++)
+            {
+                pt = CommonUtils.Point2dAddPoint2d(pt, edges[j].Start);
+            }
+
+            var ptCen = pt / edges.Count;
+            return ptCen;
+        }
+
+        public static Point2d CalculateLineSegment2dPos(List<LineSegment2d> edges)
+        {
+            var pt = edges.First().StartPoint;
+            for (int j = 1; j < edges.Count; j++)
+            {
+                pt = CommonUtils.Point2dAddPoint2d(pt, edges[j].StartPoint);
+            }
+
+            var ptCen = pt / edges.Count;
+            return ptCen;
         }
 
         /// <summary>
@@ -154,7 +251,7 @@ namespace ThRoomBoundary.topo
         /// <param name="ptSecond"></param>
         /// <param name="tolerance"></param>
         /// <returns></returns>
-        public static bool Point2dIsEqualPoint2d(Point2d ptFirst, Point2d ptSecond, double tolerance = 1e-3)
+        public static bool Point2dIsEqualPoint2d(Point2d ptFirst, Point2d ptSecond, double tolerance = 1e-6)
         {
             if (ptFirst.GetDistanceTo(ptSecond) < tolerance)
                 return true;
@@ -204,6 +301,111 @@ namespace ThRoomBoundary.topo
                 return true;
 
             return false;
+        }
+
+        /// <summary>
+        /// 点在线段上面
+        /// </summary>
+        /// <param name="point"></param>
+        /// <param name="line"></param>
+        /// <param name="tole"></param>
+        /// <returns></returns>
+        public static bool IsPointOnSegment(Point2d point, LineSegment2d line, double tole = 1e-8)
+        {
+            var ptS = line.StartPoint;
+            var ptE = line.EndPoint;
+            var lengthS = (ptS - point).Length;
+            var lengthE = (ptE - point).Length;
+            var lengthDiff = lengthS + lengthE - (ptS - ptE).Length;
+            if (CommonUtils.IsAlmostNearZero(lengthDiff, tole))
+                return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// 删除共边
+        /// </summary>
+        /// <param name="lines"></param>
+        /// <param name="outLines"></param>
+        public static void RemoveCollinearLines(List<LineSegment2d> lines, out List<LineSegment2d> outLines)
+        {
+            outLines = new List<LineSegment2d>();
+            var eraseLines = CoEdgeErase.MakeCoEdgeErase(lines);
+            outLines.AddRange(eraseLines);
+        }
+
+        /// <summary>
+        /// 空间夹角计算
+        /// </summary>
+        /// <param name="dir1"></param>
+        /// <param name="dir2"></param>
+        /// <returns></returns>
+        public static double CalAngle(XY dir1, XY dir2)
+        {
+            double val = dir1.X * dir2.X + dir1.Y * dir2.Y;
+            double tmp = Math.Sqrt(Math.Pow(dir1.X, 2) + Math.Pow(dir1.Y, 2)) * Math.Sqrt(Math.Pow(dir2.X, 2) + Math.Pow(dir2.Y, 2));
+            double angleRad = Math.Acos(CommonUtils.CutRadRange(val / tmp));
+            return angleRad;
+        }
+
+        /// <summary>
+        /// 平移
+        /// </summary>
+        /// <param name="line"></param>
+        /// <param name="vec"></param>
+        /// <returns></returns>
+        public static LineSegment2d LineAddVector(LineSegment2d line, Vector2d vec)
+        {
+            var ptS = line.StartPoint;
+            var ptE = line.EndPoint;
+            var ptSadd = ptS + vec;
+            var ptEadd = ptE + vec;
+            return new LineSegment2d(ptSadd, ptEadd);
+        }
+
+        /// <summary>
+        /// 平移
+        /// </summary>
+        /// <param name="line"></param>
+        /// <param name="vec"></param>
+        /// <returns></returns>
+        public static LineSegment2d LineDecVector(LineSegment2d line, Vector2d vec)
+        {
+            var ptS = line.StartPoint;
+            var ptE = line.EndPoint;
+            var ptSadd = ptS - vec;
+            var ptEadd = ptE - vec;
+            return new LineSegment2d(ptSadd, ptEadd);
+        }
+
+        public static TopoEdge LineDecVector(TopoEdge edge, Vector2d vec)
+        {
+            var ptS = edge.Start;
+            var ptE = edge.End;
+            var ptSadd = ptS - vec;
+            var ptEadd = ptE - vec;
+            return new TopoEdge(ptSadd, ptEadd, CommonUtils.Vector2XY(ptSadd - ptEadd));
+        }
+
+        public static XY Vector2XY(Vector2d vec)
+        {
+            return new XY(vec.X, vec.Y);
+        }
+
+        public static List<Curve> line2d2Curves(List<LineSegment2d> lines)
+        {
+            var curves = new List<Curve>();
+            foreach (var line in lines)
+            {
+                var ptS = line.StartPoint;
+                var ptE = line.EndPoint;
+                var ptS3d = new Point3d(ptS.X, ptS.Y, 0);
+                var ptE3d = new Point3d(ptE.X, ptE.Y, 0);
+                curves.Add(new Line(ptS3d, ptE3d));
+            }
+
+            return curves;
         }
     }
 }
