1fca1b2a5a70642b220d9d93ac274f1dea8ba7af
liugs20190909 <trasure@126.com>
2019-10-17 14:19:23 +0800

    ThXClip 删除了一个Test命令

--- App/ThXClip/ThXClipApp.cs
+++ App/ThXClip/ThXClipApp.cs
@@ -102,27 +102,5 @@ namespace ThXClip
             analyseRelation.EraseBlockAndItsExplodedObjs();
             ThXClipCadOperation.LockedLayers(lockedLayerNames);
         }
-
-        [CommandMethod("TIANHUACAD", "ThSpline", CommandFlags.Modal)]        
-        public void ThSpline()
-        {
-            Document doc = Application.DocumentManager.MdiActiveDocument;
-            Point3dCollection pts = new Point3dCollection();
-            pts.Add(new Point3d(1, 1, 0));
-            pts.Add(new Point3d(5, 5, 0));
-            pts.Add(new Point3d(10, 0, 0));
-
-            Vector3d startTan = new Vector3d(0.5, 0.5, 0);
-            Vector3d endTan = new Vector3d(0.5, 0.5, 0);
-            Spline acSpline = new Spline(pts, startTan, endTan, 4, 0);
-            using (Transaction trans=doc.TransactionManager.StartTransaction())
-            {
-                BlockTable bt = trans.GetObject(doc.Database.BlockTableId,OpenMode.ForRead) as BlockTable;
-                BlockTableRecord btr = trans.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForRead) as BlockTableRecord;
-                btr.AppendEntity(acSpline);
-                trans.AddNewlyCreatedDBObject(acSpline, true);
-                trans.Commit();
-            }
-        }
     }
 }
