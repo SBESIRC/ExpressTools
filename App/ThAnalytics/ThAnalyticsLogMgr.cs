2bf20f046a4ba72c926bb32e5b85cccefa1c3f71
Jerry Dong <shichongdong80@163.com>
2019-10-30 15:44:17 +0800

    Check current state in login/logout

--- App/ThAnalytics/ThAnalyticsLogMgr.cs
+++ App/ThAnalytics/ThAnalyticsLogMgr.cs
@@ -13,27 +13,41 @@ namespace ThAnalytics.Identity
 
         public static void Login()
         {
-            using (var dlg = new ThAnalyticsLoginDlg())
+            if (!IsLogged())
             {
-                if (AcadApp.ShowModalDialog(dlg) != DialogResult.OK)
+                using (var dlg = new ThAnalyticsLoginDlg())
                 {
-                    return;
-                }
+                    if (AcadApp.ShowModalDialog(dlg) != DialogResult.OK)
+                    {
+                        return;
+                    }
 
-                if (!THRecordingService.SignIn(dlg.User, dlg.Password))
-                {
-                    return;
-                }
+                    if (!THRecordingService.SignIn(dlg.User, dlg.Password))
+                    {
+                        return;
+                    }
 
-                Properties.Settings.Default.Token = THRecordingService.m_ToKen;
-                Properties.Settings.Default.Save();
+                    // 开启会话
+                    THRecordingService.SessionBegin();
+
+                    // 保存口令
+                    Properties.Settings.Default.Token = THRecordingService.m_ToKen;
+                    Properties.Settings.Default.Save();
+                }
             }
         }
 
         public static void Logout()
         {
-            Properties.Settings.Default.Token = null;
-            Properties.Settings.Default.Save();
+            if (IsLogged())
+            {
+                // 结束会话
+                THRecordingService.SessionEnd();
+
+                // 清除口令
+                Properties.Settings.Default.Token = null;
+                Properties.Settings.Default.Save();
+            }
         }
     }
 }
