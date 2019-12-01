755b8ddb8a3cdcb9c915d4e388a9be24c6c5a020
Jerry Dong <shichongdong80@163.com>
2019-12-01 23:05:34 +0800

    Init ThRhinoBridge project

---
+++ App/ThRhinoBridge/ThRhinoBridge.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Autodesk.AutoCAD.Runtime;
+using Rhino.Runtime.InProcess;
+
+namespace ThRhinoBridge
+{
+    public class ThRhinoBridge : IExtensionApplication
+    {
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Terminate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
