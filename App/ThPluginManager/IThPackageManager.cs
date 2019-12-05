b1a5c8331bb8a09bf91d864d4afb78ee7d4ed348
Jerry Dong <shichongdong80@163.com>
2019-12-05 22:42:20 +0800

    Implement NuGet package manager

---
+++ App/ThPluginManager/IThPackageManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThPluginManager
+{
+    /// <summary>
+    /// Package封装类
+    /// </summary>
+    public interface IThPackage
+    {
+        //
+    }
+
+    /// <summary>
+    /// Package管理器
+    /// </summary>
+    public interface IThPackageManager
+    {
+        // 创建Package
+        void CreatePackage(IThPackage package);
+
+        // 发布Package
+        void PublishPackage(IThPackage package);
+    }
+}
---
+++ App/ThPluginManager/ThNuGetPackageManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThPluginManager
+{
+    public class ThNuGetPackageManager : IThPackageManager
+    {
+        public void CreatePackage(IThPackage package)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PublishPackage(IThPackage package)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
