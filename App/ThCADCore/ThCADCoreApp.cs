a48388d2a68157eb6d72606f7790ab71a716527d
dongj <shichongdong80@163.com>
2020-01-19 16:50:55 +0800

    Initialize ThCADCore project (#100)
    * Initialze ThCADCore with Catel.Core
    
    * .NET 4.6 targets
    
    * Remove reference to Catel.Core

---
+++ App/ThCADCore/ThCADCoreApp.cs
@@ -0,0 +1,21 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ThCADCore
+{
+    public class ThCADCoreApp : IExtensionApplication
+    {
+        public void Initialize()
+        {
+            //
+        }
+
+        public void Terminate()
+        {
+            //
+        }
+    }
+}
