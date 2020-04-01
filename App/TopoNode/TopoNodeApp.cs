4a6e0be53e29e8d008cfe74ec6517a5c7f2a8802
navy <986489314@qq.com>
2020-04-01 11:12:31 +0800

    topoNode config

---
+++ App/TopoNode/TopoNodeApp.cs
@@ -0,0 +1,35 @@
+﻿using Linq2Acad;
+using DotNetARX;
+using AcHelper;
+using System.Linq;
+using Autodesk.AutoCAD.Runtime;
+using System.Collections.Generic;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using AcadApp = Autodesk.AutoCAD.ApplicationServices.Application;
+using NFox.Cad.Collections;
+using System.Threading;
+
+namespace TopoNode
+{
+    public class TopoNodeApp
+    {
+        public void Initialize()
+        {
+
+        }
+
+        public void Terminate()
+        {
+
+        }
+
+        [CommandMethod("TopoNode", "TopoNode", CommandFlags.Modal)]
+        static public void RoadDo()
+        {
+
+        }
+    }
+}
