a3fac2c198404d5010a853e4d53744699f3064a8
dongj <shichongdong80@163.com>
2020-04-12 13:11:48 +0800

    Initialize THWSS project (#221)

---
+++ App/ThWSS/ThWSSApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Autodesk.AutoCAD.Runtime;
+
+namespace ThWSS
+{
+    public class ThWSSApp : IExtensionApplication
+    {
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Terminate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
