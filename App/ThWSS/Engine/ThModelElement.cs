b0a4e92d6d2b3a044bac5beb117731dc5341ef9f
dongj <shichongdong80@163.com>
2020-04-27 23:54:40 +0800

    Feature/th wss engine (#255)
    * Implement ThDbObjectRecognitionEngine
    
    * Implement ThSprayLayoutEngine

---
+++ App/ThWSS/Engine/ThModelElement.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThWSS.Engine
+{
+    public abstract class ThModelElement
+    {
+        public abstract Dictionary<string, object> Properties { get; set; }
+    }
+}
