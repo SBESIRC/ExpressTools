71cf3b4dfa99a8f6f9247cfbfc70ea166b42cad1
dongj <shichongdong80@163.com>
2020-05-14 10:56:35 +0800

    Implement MBB and MBC with NTS (#297)

---
+++ App/ThCADCore.Test/ThCADCoreTestApp.cs
@@ -0,0 +1,54 @@
+﻿using AcHelper;
+using Linq2Acad;
+using ThCADCore.NTS;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace ThCADCore.Test
+{
+    public class ThCADCoreTestApp : IExtensionApplication
+    {
+        public void Initialize()
+        {
+            //
+        }
+
+        public void Terminate()
+        {
+            //
+        }
+
+        [CommandMethod("TIANHUACAD", "ThMBB", CommandFlags.Modal)]
+        public void ThMBB()
+        {
+            using (AcadDatabase acadDatabase = AcadDatabase.Active())
+            {
+                var result = Active.Editor.GetEntity("请选择对象");
+                if (result.Status != PromptStatus.OK)
+                {
+                    return;
+                }
+
+                var pline = acadDatabase.Element<Polyline>(result.ObjectId);
+                acadDatabase.ModelSpace.Add(pline.MinimumBoundingBox());
+            }
+        }
+
+        [CommandMethod("TIANHUACAD", "ThMBC", CommandFlags.Modal)]
+        public void ThMBC()
+        {
+            using (AcadDatabase acadDatabase = AcadDatabase.Active())
+            {
+                var result = Active.Editor.GetEntity("请选择对象");
+                if (result.Status != PromptStatus.OK)
+                {
+                    return;
+                }
+
+                var pline = acadDatabase.Element<Polyline>(result.ObjectId);
+                acadDatabase.ModelSpace.Add(pline.MinimumBoundingCircle());
+            }
+        }
+    }
+}
