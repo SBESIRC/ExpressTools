881317273282d444201c22c6e36b718d84deb61f
Jerry Dong <shichongdong80@163.com>
2020-05-27 22:32:43 +0800

    Fix XrefFileLock.LockFile() eFileAcessErr exception

--- App/ThEssential/Command/ThBConvertCommand.cs
+++ App/ThEssential/Command/ThBConvertCommand.cs
@@ -94,7 +94,7 @@ namespace ThEssential.Command
                 //  3. 块引用是“Overlay”
                 // 对于每一个Xref块引用，获取其Xref数据库
                 // 一个Xref块可以有多个块引用
-                var xrefs = new List<Database>();
+                var xrefs = new List<string>();
                 foreach (ObjectId obj in objs)
                 {
                     var blkRef = currentDb.Element<BlockReference>(obj);
@@ -102,7 +102,12 @@ namespace ThEssential.Command
                     if (blkDef.IsFromExternalReference && blkDef.IsFromOverlayReference)
                     {
                         // 暂时不考虑unresolved的情况
-                        xrefs.Add(blkDef.GetXrefDatabase(false));
+                        var xrefDatatbase = blkDef.GetXrefDatabase(false);
+                        // 暂时只关心特定的外参图纸（暖通提资）
+                        if (Path.GetFileName(xrefDatatbase.Filename).StartsWith("H"))
+                        {
+                            xrefs.Add(xrefDatatbase.Filename);
+                        }
                     }
                 }
 
@@ -112,75 +117,76 @@ namespace ThEssential.Command
                 // 并将源块引用中的属性”刷“到新的块引用
                 foreach (var xref in xrefs)
                 {
-                    using (var xf = XrefFileLock.LockFile(xref.XrefBlockId))
+                    // 在协同环境下，外参可能是不可写的
+                    // 在外参不可读的情况下，XrefFileLock.LockFile()会抛出异常
+                    // 具体的情况可以参阅这篇文章：
+                    //  https://www.keanw.com/2015/01/modifying-the-contents-of-an-autocad-xref-using-net.html
+                    // 考虑到我们这里的情况，并不需要对外参进行写操作，我们可以直接以“只读”的方式打开外参
+                    // 从而绕过XrefFileLock.LockFile()的异常
+                    using (AcadDatabase xrefDb = AcadDatabase.Open(xref, DwgOpenMode.ReadOnly, false))
                     {
-                        xref.RestoreOriginalXrefSymbols();
-                        if (Path.GetFileName(xref.OriginalFileName).StartsWith("H"))
+                        foreach (var rule in manager.Rules)
                         {
-                            foreach (var rule in manager.Rules)
+                            var block = rule.Transformation.Item1;
+                            var visibility = block.StringValue(ThBConvertCommon.BLOCK_MAP_ATTRIBUTES_BLOCK_VISIBILITY);
+                            foreach (ObjectId blkRef in xrefDb.Database.GetBlockReferences(block, extents))
                             {
-                                var block = rule.Transformation.Item1;
-                                var visibility = block.StringValue(ThBConvertCommon.BLOCK_MAP_ATTRIBUTES_BLOCK_VISIBILITY);
-                                foreach (ObjectId blkRef in xref.GetBlockReferences(block, extents))
+                                try
                                 {
-                                    try
-                                    {
-                                        // 根据块引用的“块名”，匹配转换后的块定义的信息
-                                        var blockReference = blkRef.Database.GetBlockReference(blkRef);
-                                        if (blockReference.CurrentVisibilityStateValue() != visibility)
-                                        {
-                                            continue;
-                                        }
+                                    // 根据块引用的“块名”，匹配转换后的块定义的信息
+                                    var blockReference = blkRef.Database.GetBlockReference(blkRef);
 
-                                        ThBlockConvertBlock transformedBlock = null;
-                                        if (Mode == ConvertMode.STRONGCURRENT)
-                                        {
-                                            transformedBlock = manager.TransformRule(blockReference.EffectiveName);
-                                        }
-                                        else if (Mode == ConvertMode.WEAKCURRENT)
+                                    // 获取转换后的块信息
+                                    ThBlockConvertBlock transformedBlock = null;
+                                    if (Mode == ConvertMode.STRONGCURRENT)
+                                    {
+                                        transformedBlock = manager.TransformRule(blockReference.EffectiveName);
+                                    }
+                                    else if (Mode == ConvertMode.WEAKCURRENT)
+                                    {
+                                        if (blockReference.CurrentVisibilityStateValue() == visibility)
                                         {
                                             transformedBlock = manager.TransformRule(
                                                 blockReference.EffectiveName,
                                                 blockReference.CurrentVisibilityStateValue());
                                         }
-                                        else
-                                        {
-                                            throw new NotSupportedException();
-                                        }
-                                        if (transformedBlock == null)
-                                        {
-                                            continue;
-                                        }
-
-                                        // 在当前图纸中查找是否存在新的块定义
-                                        // 若不存在，则插入新的块定义；
-                                        // 若存在，则保持现有的块定义
-                                        var name = (string)transformedBlock.Attributes[ThBConvertCommon.BLOCK_MAP_ATTRIBUTES_BLOCK];
-                                        var result = currentDb.Blocks.Import(blockDb.Blocks.ElementOrDefault(name), false);
-
-                                        // 插入新的块引用
-                                        var objId = currentDb.ModelSpace.ObjectId.InsertBlockReference(
-                                            "0",
-                                            name,
-                                            Point3d.Origin,
-                                            new Scale3d(transformedBlock.Scale()),
-                                            0.0,
-                                            new Dictionary<string, string>());
-
-                                        // 将新插入的块引用调整到源块引用所在的位置
-                                        engine.TransformBy(objId, blockReference);
-
-                                        // 将源块引用的属性“刷”到新的块引用
-                                        engine.MatchProperties(objId, blockReference);
                                     }
-                                    catch
+                                    else
+                                    {
+                                        throw new NotSupportedException();
+                                    }
+                                    if (transformedBlock == null)
                                     {
                                         continue;
                                     }
+
+                                    // 在当前图纸中查找是否存在新的块定义
+                                    // 若不存在，则插入新的块定义；
+                                    // 若存在，则保持现有的块定义
+                                    var name = (string)transformedBlock.Attributes[ThBConvertCommon.BLOCK_MAP_ATTRIBUTES_BLOCK];
+                                    var result = currentDb.Blocks.Import(blockDb.Blocks.ElementOrDefault(name), false);
+
+                                    // 插入新的块引用
+                                    var objId = currentDb.ModelSpace.ObjectId.InsertBlockReference(
+                                        "0",
+                                        name,
+                                        Point3d.Origin,
+                                        new Scale3d(transformedBlock.Scale()),
+                                        0.0,
+                                        new Dictionary<string, string>());
+
+                                    // 将新插入的块引用调整到源块引用所在的位置
+                                    engine.TransformBy(objId, blockReference);
+
+                                    // 将源块引用的属性“刷”到新的块引用
+                                    engine.MatchProperties(objId, blockReference);
+                                }
+                                catch
+                                {
+                                    continue;
                                 }
                             }
                         }
-                        xref.RestoreForwardingXrefSymbols();
                     }
                 }
             }
