c425ba2f86fc6a422143a4125627f879d7479026
liugs <trasure@126.com>
2020-05-28 14:59:47 +0800

    Create ThStructure Solution

---
+++ App/ThStructureCheck/ThStructureCheck.ThBeamInfo/ThBeamInfoApp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ThStructureCheck.ThBeamInfo
+{
+    public class ThBeamInfoApp
+    {
+    }
+}
