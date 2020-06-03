db751d17166cfc3b90107a4d29c9f98c57f11945
coconavy <986489314@qq.com>
2020-06-03 13:24:38 +0800

    progress (#339)

--- App/TopoNode/Progress/Progress.Designer.cs
+++ App/TopoNode/Progress/Progress.Designer.cs
@@ -31,14 +31,16 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Progress));
             this.m_progressBar = new System.Windows.Forms.ProgressBar();
             this.m_lblTip = new System.Windows.Forms.Label();
+            this.m_lblProgresss = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // m_progressBar
             // 
-            this.m_progressBar.Location = new System.Drawing.Point(55, 35);
+            this.m_progressBar.ForeColor = System.Drawing.SystemColors.Control;
+            this.m_progressBar.Location = new System.Drawing.Point(55, 37);
             this.m_progressBar.Maximum = 1000;
             this.m_progressBar.Name = "m_progressBar";
-            this.m_progressBar.Size = new System.Drawing.Size(341, 22);
+            this.m_progressBar.Size = new System.Drawing.Size(341, 20);
             this.m_progressBar.Step = 1;
             this.m_progressBar.TabIndex = 0;
             // 
@@ -51,11 +53,26 @@
             this.m_lblTip.TabIndex = 1;
             this.m_lblTip.Text = "图纸预处理...";
             // 
+            // m_lblProgresss
+            // 
+            this.m_lblProgresss.AutoSize = true;
+            this.m_lblProgresss.BackColor = System.Drawing.SystemColors.Control;
+            this.m_lblProgresss.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.m_lblProgresss.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.m_lblProgresss.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.m_lblProgresss.Location = new System.Drawing.Point(337, 13);
+            this.m_lblProgresss.Name = "m_lblProgresss";
+            this.m_lblProgresss.Size = new System.Drawing.Size(56, 16);
+            this.m_lblProgresss.TabIndex = 2;
+            this.m_lblProgresss.Text = "10%100";
+            this.m_lblProgresss.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Progress
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(440, 87);
+            this.Controls.Add(this.m_lblProgresss);
             this.Controls.Add(this.m_lblTip);
             this.Controls.Add(this.m_progressBar);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -74,5 +91,6 @@
 
         private System.Windows.Forms.ProgressBar m_progressBar;
         private System.Windows.Forms.Label m_lblTip;
+        public System.Windows.Forms.Label m_lblProgresss;
     }
 }
