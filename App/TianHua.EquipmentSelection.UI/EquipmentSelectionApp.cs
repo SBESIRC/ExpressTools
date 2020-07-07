fcb41280b52edb8d5c04b2266370306d0bde15c2
Jerry Dong <shichongdong80@163.com>
2020-07-07 18:37:52 +0800

    Implement THEQSEL command

---
+++ App/TianHua.EquipmentSelection.UI/EquipmentSelectionApp.cs
@@ -0,0 +1,27 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using AcadApp = Autodesk.AutoCAD.ApplicationServices.Application;
+
+namespace TianHua.FanSelection.UI
+{
+    public class EquipmentSelectionApp : IExtensionApplication
+    {
+        public void Initialize()
+        {
+            //
+        }
+
+        public void Terminate()
+        {
+            //
+        }
+
+        [CommandMethod("TIANHUACAD", "THEQSEL", CommandFlags.Modal)]
+        public void ThEquipmentSelection()
+        {
+            using (var dlg = new fmFanSelection())
+            {
+                AcadApp.ShowModelessDialog(dlg);
+            }
+        }
+    }
+}
