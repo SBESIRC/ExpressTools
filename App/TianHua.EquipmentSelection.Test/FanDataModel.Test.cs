f793aea01c0068713bef040a8aa461e778d31b40
Jerry Dong <shichongdong80@163.com>
2020-08-26 16:28:44 +0800

    Using NUnit framework

--- App/TianHua.EquipmentSelection.Test/FanDataModel.Test.cs
+++ App/TianHua.EquipmentSelection.Test/FanDataModel.Test.cs
@@ -1,21 +1,23 @@
-﻿using Xunit;
+﻿using NUnit.Framework;
 using TianHua.FanSelection;
 
 namespace TianHua.EquipmentSelection.Test
 {
+    [TestFixture]
     public class FanDataModelTest
     {
-        [Fact]
+        [Test]
         public void FanNumberTest()
         {
             var model = new FanDataModel()
             {
+                PID = "0",
                 InstallSpace = "4",
                 InstallFloor = "F2",
                 VentNum = "2",
                 Scenario = "平时送风"
             };
-            Assert.Equal("EF-4-F2-2", model.GetFanNum());
+            Assert.AreEqual("SF-4-F2-2", model.GetFanNum());
         }
     }
 }
