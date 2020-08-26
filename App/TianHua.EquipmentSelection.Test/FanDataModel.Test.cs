24fc095705532edc2bc1d596fa08bf83a47dd7f0
Jerry Dong <shichongdong80@163.com>
2020-08-26 15:57:57 +0800

    Add xunit.runner.visualstudio

--- App/TianHua.EquipmentSelection.Test/FanDataModel.Test.cs
+++ App/TianHua.EquipmentSelection.Test/FanDataModel.Test.cs
@@ -1,24 +1,21 @@
 ﻿using Xunit;
+using TianHua.FanSelection;
 
 namespace TianHua.EquipmentSelection.Test
 {
     public class FanDataModelTest
     {
         [Fact]
-        public void PassingTest()
+        public void FanNumberTest()
         {
-            Assert.Equal(4, Add(2, 2));
-        }
-
-        [Fact]
-        public void FailingTest()
-        {
-            Assert.Equal(5, Add(2, 2));
-        }
-
-        int Add(int x, int y)
-        {
-            return x + y;
+            var model = new FanDataModel()
+            {
+                InstallSpace = "4",
+                InstallFloor = "F2",
+                VentNum = "2",
+                Scenario = "平时送风"
+            };
+            Assert.Equal("EF-4-F2-2", model.GetFanNum());
         }
     }
 }
