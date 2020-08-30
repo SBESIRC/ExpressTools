7b531b45b4fa8c8e9b0684e2080da1c35cfd2061
Jerry Dong <shichongdong80@163.com>
2020-08-30 10:45:00 +0800

    Hook up FluentValidation

---
+++ App/TianHua.EquipmentSelection/Validation/FanDataModelValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace TianHua.FanSelection.Validation
+{
+    public class FanDataModelValidator : AbstractValidator<FanDataModel>
+    {
+        public FanDataModelValidator()
+        {
+            //
+        }
+    }
+}
