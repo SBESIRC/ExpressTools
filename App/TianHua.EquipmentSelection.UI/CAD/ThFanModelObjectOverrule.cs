424e89dce01bafabdddfbfb593c194b970c52abf
Jerry Dong <shichongdong80@163.com>
2020-10-23 15:08:44 +0800

    Define ThFanModelObjectOverrule

---
+++ App/TianHua.EquipmentSelection.UI/CAD/ThFanModelObjectOverrule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace TianHua.FanSelection.UI.CAD
+{
+    public class ThFanModelObjectOverrule : ObjectOverrule
+    {
+        public override void Erase(DBObject dbObject, bool erasing)
+        {
+            //
+        }
+
+        public override DBObject DeepClone(DBObject dbObject, DBObject ownerObject, IdMapping idMap, bool isPrimary)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DBObject WblockClone(DBObject dbObject, RXObject ownerObject, IdMapping idMap, bool isPrimary)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
